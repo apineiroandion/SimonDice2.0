b664888de4e7984159cec97ca5c9a560e6187e85
apineiroandion <apineiroandion@danielcastelao.org>
2024-11-27 12:55:44 +0100

    MyViewModel comentada

--- app/src/main/java/com/example/simondice20/MyViewModel.kt
+++ app/src/main/java/com/example/simondice20/MyViewModel.kt
@@ -12,29 +12,54 @@ import com.example.simondice20.Datos.secuenciaMaquina
 import com.example.simondice20.Datos.toastText
 import com.example.simondice20.Estados.*
 
-
+/**
+ * Clase ViewModel para la lógica de la aplicación
+ */
 class MyViewModel: ViewModel() {
+    /**
+     * LiveData para el estado de la aplicación
+     */
     var estadoLiveData: MutableLiveData<Estados> = MutableLiveData(ESPERANDO)
 
+    /**
+     * Función para aumentar la ronda
+     */
     private fun aumentarRonda() {
         ronda.value += 1
     }
+
+    /**
+     * Función para generar la secuencia de la máquina
+     */
     fun generarSecuencia() {
         aumentarRonda()
         secuenciaMaquina.add((0..3).random())
         isPrinted.value = false
         estadoGenerando()
     }
+
+    /**
+     * Función que inicia la comprobación de la secuencia
+     */
     fun click(buttonId: Int, context: Context) {
         secuenciaJugador.add(buttonId)
         comprobacion(context)
 
     }
+
+    /**
+     * Funcion que lanza la comprobacion si la secuecia jugador es igual o menor
+     * a la secuencia maquina
+     */
     private fun comprobacion(context: Context) {
         if (secuenciaJugador.size <= secuenciaMaquina.size) {
             comprobarSecuencia(context)
         }
     }
+
+    /**
+     * Función para comprobar si la secuencia del jugador es correcta
+     */
     private fun comprobarSecuencia(context: Context) {
         if (secuenciaMaquina == secuenciaJugador){
             secuenciaJugador.clear()
@@ -58,18 +83,30 @@ class MyViewModel: ViewModel() {
         }
     }
 
+    /**
+     * Función para establecer el texto del toast
+     */
     private fun setToastText(text: String) {
         toastText = text
     }
 
+    /**
+     * Función para establecer el estado de la aplicación a GENERANDO
+     */
     private fun estadoGenerando() {
         estadoLiveData.value = GENERANDO
     }
 
+    /**
+     * Función para establecer el estado de la aplicación a JUGANDO
+     */
     fun estadoJugando() {
         estadoLiveData.value = JUGANDO
     }
 
+    /**
+     * Función para establecer el estado de la aplicación a ESPERANDO
+     */
     private fun estadoEsperando() {
         estadoLiveData.value = ESPERANDO
     }
